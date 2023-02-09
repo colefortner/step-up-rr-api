fab26bbe722cc3ac7b709825dc26a16ca97074ed
colefortner <colefortner@gmail.com>
2023-02-08 23:57:15 -0500

    Add User model validation, password digest, installed bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_033859) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_043148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_033859) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
